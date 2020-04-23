69ee428b3d9042cd623d770ba4bd00b0ef809a66
peterrobert <pwambui93@gmail.com>
2020-04-23 20:10:35 +0300

    finished with edit create destroy and flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
     def show
         
         @article = Article.find(params[:id])
+        
 
     end
 
@@ -18,13 +19,31 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
+
         redirect_to article_path(@article)
+        
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.delete
+        flash.notice = "Article '#{@article.title}' Removed!"
         redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
+
     end
     
 end
